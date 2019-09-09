80b45282e08156b2acac5f3b3ec525b66a063cd2
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-09-09 16:18:59 +0200

    change train_cvae

--- scripts/train_cvae.py
+++ scripts/train_cvae.py
@@ -2,37 +2,12 @@ import sys
 
 import numpy as np
 import scanpy as sc
-from sklearn.preprocessing import LabelEncoder
 
 from reptrvae.models import CVAE
+from reptrvae.plotting import plot_umap
+from reptrvae.utils import train_test_split, label_encoder
 
 data_name = sys.argv[1]
-
-
-def train_test_split(adata, train_frac=0.85):
-    train_size = int(adata.shape[0] * train_frac)
-    indices = np.arange(adata.shape[0])
-    np.random.shuffle(indices)
-    train_idx = indices[:train_size]
-    test_idx = indices[train_size:]
-
-    train_data = adata[train_idx, :]
-    valid_data = adata[test_idx, :]
-
-    return train_data, valid_data
-
-
-def label_encoder(adata, label_encoder=None, condition_key='condition'):
-    if label_encoder is None:
-        le = LabelEncoder()
-        labels = le.fit_transform(adata.obs[condition_key].tolist())
-    else:
-        le = None
-        labels = np.zeros(adata.shape[0])
-        for condition, label in label_encoder.items():
-            labels[adata.obs[condition_key] == condition] = label
-    return labels.reshape(-1, 1), le
-
 
 if data_name == "haber":
     keys = ["Control", "Hpoly.Day10"]
@@ -62,6 +37,8 @@ elif data_name == "kang":
     target_condition = "STIM"
     target_conditions = ['STIM']
     le = {"CTRL": 0, "STIM": 1}
+else:
+    raise Exception("Invalid data name")
 
 adata = sc.read(f"./data/{data_name}/{data_name}_normalized.h5ad")
 adata = adata.copy()[adata.obs[condition_key].isin(keys)]
@@ -79,83 +56,39 @@ net_valid_adata = valid_adata[
     ~((valid_adata.obs[cell_type_key] == specific_cell_type) & (
         valid_adata.obs[condition_key].isin(target_conditions)))]
 
-z_dim = 20
+z_dim = 40
 network = CVAE(x_dimension=net_train_adata.X.shape[1],
                z_dimension=z_dim,
                alpha=0.1,
                model_path=f"../models/CVAE/{data_name}/{specific_cell_type}/cvae")
-if sys.argv[2] == 'scripts':
-    network.train(net_train_adata,
-                  use_validation=True,
-                  valid_data=net_valid_adata,
-                  n_epochs=300)
 
-    train_labels, _ = label_encoder(train_adata, le, 'condition')
-    cell_type_adata = train_adata[train_adata.obs[cell_type_key] == specific_cell_type]
+network.train(net_train_adata,
+              use_validation=True,
+              valid_data=net_valid_adata,
+              n_epochs=300)
 
-    unperturbed_data = cell_type_adata[cell_type_adata.obs[condition_key] == control_condition]
-    target_labels = np.zeros((len(unperturbed_data), 1)) + le[target_condition]
-    predicted_cells = network.predict(unperturbed_data, target_labels)
-    pred_adata = sc.AnnData(predicted_cells,
-                            obs={condition_key: [f"{specific_cell_type}_pred_{target_condition}"] * len(target_labels)})
-    pred_adata.obs['method'] = 'CVAE'
-    pred_adata.obs[cell_type_key] = specific_cell_type
-    pred_adata.var_names = cell_type_adata.var_names
-    all_adata = cell_type_adata.concatenate(pred_adata)
+train_labels, _ = label_encoder(train_adata, le, 'condition')
+cell_type_adata = train_adata[train_adata.obs[cell_type_key] == specific_cell_type]
 
-    all_adata.write(f"../data/reconstructed/{data_name}/CVAE-{specific_cell_type}.h5ad")
-    print("Model has been trained")
-    sc.settings.figdir = "../results/kang/"
-    labels, _ = label_encoder(adata, condition_key=condition_key)
+unperturbed_data = cell_type_adata[cell_type_adata.obs[condition_key] == control_condition]
+target_labels = np.zeros((len(unperturbed_data), 1)) + le[target_condition]
+predicted_cells = network.predict(unperturbed_data, target_labels)
+pred_adata = sc.AnnData(predicted_cells,
+                        obs={condition_key: [f"{specific_cell_type}_pred_{target_condition}"] * len(target_labels)})
+pred_adata.obs['method'] = 'CVAE'
+pred_adata.obs[cell_type_key] = specific_cell_type
+pred_adata.var_names = cell_type_adata.var_names
+all_adata = cell_type_adata.concatenate(pred_adata)
 
-    latent = network.to_latent(adata.X, labels)
-    latent_adata = sc.AnnData(X=latent)
-    latent_adata.obs = adata.obs.copy(deep=True)
+all_adata.write(f"../data/reconstructed/{data_name}/CVAE-{specific_cell_type}.h5ad")
+print("Model has been trained")
+sc.settings.figdir = "../results/kang/"
+labels, _ = label_encoder(adata, condition_key=condition_key)
 
-    mmd_latent = network.to_mmd_layer(adata.X, labels)
-    mmd_latent_adata = sc.AnnData(X=mmd_latent)
-    mmd_latent_adata.obs = adata.obs.copy(deep=True)
+latent_adata = network.to_latent(adata.X, labels)
+mmd_latent_adata = network.to_mmd_layer(adata.X, labels)
 
-    print("Latents has been computed")
-
-    sc.pp.neighbors(latent_adata)
-    sc.tl.umap(latent_adata)
-    sc.pl.umap(latent_adata, color=condition_key, frameon=False,
-               save='_CVAE_latent_condition.pdf')
-    sc.pl.umap(latent_adata, color=cell_type_key, frameon=False,
-               save='_CVAE_latent_celltype.pdf')
-
-    sc.pp.neighbors(mmd_latent_adata)
-    sc.tl.umap(mmd_latent_adata)
-    sc.pl.umap(mmd_latent_adata, color=condition_key, frameon=False,
-               save='_CVAE_MMD_latent_condition.pdf')
-    sc.pl.umap(mmd_latent_adata, color=cell_type_key, frameon=False,
-               save='_CVAE_MMD_latent_celltypes.pdf')
-    print("Latents has been plotted and saved")
-
-else:
-    sc.settings.figdir = "../results/kang/"
-    network.restore_model()
-    labels, _ = label_encoder(adata, condition_key=condition_key)
-
-    latent = network.to_latent(adata.X, labels)
-    latent_adata = sc.AnnData(X=latent)
-    latent_adata.obs = adata.obs.copy(deep=True)
-
-    mmd_latent = network.to_mmd_layer(adata.X, labels)
-    mmd_latent_adata = sc.AnnData(X=mmd_latent)
-    mmd_latent_adata.obs = adata.obs.copy(deep=True)
-
-    sc.pp.neighbors(latent_adata)
-    sc.tl.umap(latent_adata)
-    sc.pl.umap(latent_adata, color=condition_key, frameon=False,
-               save='_CVAE_latent_condition.pdf')
-    sc.pl.umap(latent_adata, color=cell_type_key, frameon=False,
-               save='_CVAE_latent_celltype.pdf')
-
-    sc.pp.neighbors(mmd_latent_adata)
-    sc.tl.umap(mmd_latent_adata)
-    sc.pl.umap(mmd_latent_adata, color=condition_key, frameon=False,
-               save='_CVAE_MMD_latent_condition.pdf')
-    sc.pl.umap(mmd_latent_adata, color=cell_type_key, frameon=False,
-               save='_CVAE_MMD_latent_celltypes.pdf')
+print("Latents has been computed")
+plot_umap(mmd_latent_adata, condition_key, cell_type_key, False,
+          path_to_save=f"./results/{data_name}/", model_name="CVAE_MMD")
+print("Latents has been plotted and saved")
